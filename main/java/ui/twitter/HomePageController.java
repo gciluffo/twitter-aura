1397253520e443be6ee2f44e2451e9bda4f76d0c
Griffin Ciluffo <griffin@ciluffo.com>
2016-02-29 20:48:27 -0700

    Added tweet class

--- main/java/ui/twitter/HomePageController.java
+++ main/java/ui/twitter/HomePageController.java
@@ -2,6 +2,7 @@ package ui.twitter;
 
 import java.sql.PreparedStatement;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -31,15 +32,29 @@ public class HomePageController {
     @AuraEnabled
     public static String getTweets() throws Exception {
 
+        ArrayList<String> returnStuff = new ArrayList<String>();
+        try {
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:~/Twitter", "gciluffo", "Gooner55");
+        boolean dropResult = conn.prepareStatement("DROP TABLE IF EXISTS TWEETS").execute();
+        boolean createResult = conn.prepareStatement("CREATE TABLE TWEETS (ID INT PRIMARY KEY, NAME VARCHAR(255) )").execute();
+        boolean insertResult = conn.prepareStatement("INSERT INTO TWEETS VALUES (1, 'A tweet from the database')").execute();
+
         ResultSet rs = conn.prepareStatement("SELECT * FROM TWEETS").executeQuery();
 
+        while (rs.next()) {
+            // Get each column and put them into new objects into the list
+            returnedStuff.append(new Tweet(rs.getString(1), rs.getString(2), "Image", "Today"));
+        }
         // Just return the first entry from query
         String something = rs.getString(1);
         //return "Merp"
         conn.close();
    
         return something;
-    }
+       }
+       catch(Exception e) {
+        return Arrays.toString(e.getStackTrace());
+       }
+  }
 }
